4450ab1ed3c7efbef2ec5d59c2a6e04bdb7277df
朱亮 <teamide@163.com>
2026-05-18 10:20:37 +0800

    提交

--- cmd/build-amd64.sh
+++ cmd/build-amd64.sh
@@ -5,7 +5,8 @@ set -ex;
 # 移至脚本目录
 cd /build
 
-echo `pwd`
+currentDir=$(pwd)
+echo "当前目录：$currentDir"
 
 echo "set go env GOFLAGS"
 go env -w GOFLAGS="-buildvcs=false"
@@ -14,13 +15,13 @@ go mod tidy
 
 echo "build start"
 
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go \
+CGO_ENABLED=1 GOOS=linux GOARCH=amd64 CGO_CFLAGS="-D_LARGEFILE64_SOURCE" go \
 build -ldflags "-s" -o /build/build-linux-amd64 .
 
 echo "build success"
 
 echo "build check version"
 
-export LD_LIBRARY_PATH=$(pwd)/lib/shentong/amd64/
+export LD_LIBRARY_PATH="$currentDir/lib/shentong/amd64/"
 
 /build/build-linux-amd64 -v
--- cmd/build-arm64.sh
+++ cmd/build-arm64.sh
@@ -5,7 +5,8 @@ set -ex;
 # 移至脚本目录
 cd /build
 
-echo `pwd`
+currentDir=$(pwd)
+echo "当前目录：$currentDir"
 
 echo "set go env GOFLAGS"
 go env -w GOFLAGS="-buildvcs=false"
@@ -14,13 +15,13 @@ go mod tidy
 
 echo "build start"
 
-CGO_ENABLED=1 GOOS=linux GOARCH=arm64 go \
+CGO_ENABLED=1 GOOS=linux GOARCH=arm64 CGO_CFLAGS="-D_LARGEFILE64_SOURCE" go \
 build -ldflags "-s" -o /build/build-linux-arm64 .
 
 echo "build success"
 
 echo "build check version"
 
-export LD_LIBRARY_PATH=$(pwd)/lib/shentong/arm64/
+export LD_LIBRARY_PATH="$currentDir/lib/shentong/arm64/"
 
 /build/build-linux-arm64 -v
